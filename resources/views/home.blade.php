fe738a749497002817934e3b2f5d83af513b8171
Matthew Sebastian <matthew.sebastian@binus.edu>
2023-01-04 20:46:18 +0700

    Matthew Push

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <h1>HALLO</h1>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
