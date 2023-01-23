342c84ff8aad2d5097456915bdc58c31e422bfb8
Liviavia <melodyviel25@gmail.com>
2023-01-23 23:24:20 +0700

    restaurantdetail -liv

--- resources/views/Admin/manageFoodBlogger.blade.php
+++ resources/views/Admin/manageFoodBlogger.blade.php
@@ -6,16 +6,53 @@
 
 @section('body')
 
-<div class="container">
+<div class="container mt-5">
     <form action="{{route('searchFoodBlogger')}}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
         <input type="search" class="form-control" name="search_food_blogger_name" placeholder="Cari Food Blogger" aria-label="Search">
     </form>
 
     <br>
 
-    <form action="#">
-        <button type="submit" class="btn btn-primary"><i class="bi bi-plus-lg"></i> Tambah Food Blogger</button>
-    </form>
+    {{-- ADD PRODUCT blm dihubungkan dengan add page --}}
+    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+        <form action="#" style="background-color: #0C3459; border-radius:25px">
+            <button type="submit" class="btn btn-primary" style="color: #FFFFFF; font-weight:bold"> Tambah Food Blogger </button>
+        </form>
+    </div>
+
+    @foreach ( $foodBloggers as $fb )
+    <div class="card mx-auto my-auto mt-2" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-3 mx-auto my-auto ">
+                <img src="{{asset($fb->food_blogger_image)}}" class="card-img" alt="{{ $fb->food_blogger_name }}">
+            </div>
+            <div class="col-lg-8">
+                <div class="card-body">
+                    <h5 class="card-title" style="color: #0D3B66; font-weight:bold">{{ $fb->food_blogger_name}}</h5>
+                    <br>
+
+                    <div class="d-grid gap-2 d-md-flex justify-content-md">
+                        {{-- EDIT --}}
+                        {{-- blm dihubungkan dengan edit page --}}
+                        <form action="#" style="background-color: #FDB221; border-radius: 25px">
+                            <button type="submit" class="btn white-link text-decoration-none bg-warning" style="color: #0D3B66; font-weight:bold">Edit Blogger</button>
+                        </form>
+
+                        {{-- DELETE --}}
+                        {{-- blm dihubungkan dengan remove page --}}
+                        <form action="#" style="background-color: #FF0000; border-radius: 25px" method="POST">
+                            @method('delete')
+                            @csrf
+                            <button class="btn btn-danger" type="submit" style="color: #FFFFFF; font-weight:bold">
+                                Remove Blogger
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 </div>
 
 @endsection
--- resources/views/Admin/manageRestaurant.blade.php
+++ resources/views/Admin/manageRestaurant.blade.php
@@ -6,16 +6,53 @@
 
 @section('body')
 
-<div class="container">
-    <form action="{{route('searchRestaurant')}}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-        <input type="search" class="form-control" name="search_restaurant_name" placeholder="Cari Restoran" aria-label="Search">
+<div class="container mt-5">
+    <form action="{{route('searchRestaurant')}}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" >
+        <input type="search" class="form-control" name="search_restaurant_name" placeholder="Cari Restoran" aria-label="Search" >
     </form>
 
     <br>
 
-    <form action="#">
-        <button type="submit" class="btn btn-primary"><i class="bi bi-plus-lg"></i> Tambah Restoran</button>
-    </form>
+    {{-- ADD PRODUCT blm dihubungkan dengan add page --}}
+    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+        <form action="#" style="background-color: #0C3459; border-radius:25px">
+            <button type="submit" class="btn btn-primary" style="color: #FFFFFF; font-weight:bold"> Tambah Restoran </button>
+        </form>
+    </div>
+
+    @foreach ( $restaurants as $rs )
+    <div class="card mx-auto my-auto mt-2" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-3 mx-auto my-auto ">
+                <img src="{{asset($rs->restaurant_image)}}" class="card-img" alt="{{ $rs->restaurant_name }}">
+            </div>
+            <div class="col-lg-8">
+                <div class="card-body">
+                    <h5 class="card-title" style="color: #0D3B66; font-weight:bold">{{ $rs->restaurant_name}}</h5>
+                    <br>
+
+                    <div class="d-grid gap-2 d-md-flex justify-content-md">
+                        {{-- EDIT --}}
+                        {{-- blm dihubungkan dengan edit page --}}
+                        <form action="#" style="background-color: #FDB221; border-radius: 25px">
+                            <button type="submit" class="btn white-link text-decoration-none bg-warning" style="color: #0D3B66; font-weight:bold">Edit Restaurant</button>
+                        </form>
+
+                        {{-- DELETE --}}
+                        {{-- blm dihubungkan dengan remove page --}}
+                        <form action="#" style="background-color: #FF0000; border-radius: 25px" method="POST">
+                            @method('delete')
+                            @csrf
+                            <button class="btn btn-danger" type="submit" style="color: #FFFFFF; font-weight:bold">
+                                Remove Restaurant
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 </div>
 
 @endsection
